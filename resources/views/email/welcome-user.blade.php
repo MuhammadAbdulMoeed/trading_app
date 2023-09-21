f1fad18d642202a0cc43be1cb24a5ac4e2bb80c1
MudassarCIS <cis.mudassar@gmail.com>
2023-09-21 18:15:35 +0500

    update changes in code

--- resources/views/email/welcome-user.blade.php
+++ resources/views/email/welcome-user.blade.php
@@ -1,12 +1,19 @@
 @component('mail::message')
+
     Welcome to {{ config('app.name') }}
 
     Name: {{ $mailData['name'] }}
     Email: {{ $mailData['email'] }}
 
-    Visit site here <a href="http://phplaravel-1098145-3891736.cloudwaysapps.com/">{{ config('app.name') }} </a>
+    Visit Site Here :  http://phplaravel-1098145-3891736.cloudwaysapps.com
 
     Thanks
 
     Team {{ config('app.name') }}
+
+   {{-- @component('mail::button', ['url' => "http://phplaravel-1098145-3891736.cloudwaysapps.com/"])
+        Visit Site Here
+    @endcomponent--}}
+
+
 @endcomponent
